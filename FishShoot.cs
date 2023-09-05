6beee76e81f3053eb0c1c73cd1e35dbcb928e4d7
ztreasure13 <141884744+ztreasure13@users.noreply.github.com>
2023-09-05 14:49:26 -0400

    Add files via upload

---
+++ FishShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishShoot : MonoBehaviour
+{
+    public AudioClip throwSound;
+    public Rigidbody fishPrefab;
+    public float fishSpeed = 15.0f;
+    float fishTimer;
+    float fishFlyTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fishTimer = 0.0f;
+        fishFlyTime = Random.Range(2.0f, 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fishTimer += Time.deltaTime;
+        if (fishTimer > fishFlyTime)
+        {//start if 2
+            Rigidbody newFish = Instantiate(fishPrefab, transform.position, transform.rotation) as Rigidbody;
+            newFish.name = "Fish";//Naming fish
+            newFish.velocity = transform.forward * fishSpeed;
+            AudioSource.PlayClipAtPoint(throwSound, transform.position);
+            fishFlyTime = Random.Range(2.0f, 5.0f);
+            fishTimer = 0.0f;
+        }//end if 2
+    }
+}
---
+++ ThrowTriggerB.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThrowTriggerB : MonoBehaviour
+{
+    //Script that goes on the mat for the dock mini game...
+    //Variables for throwing...
+    public RawImage crosshair;
+
+    // Use this for initialization
+    void Start()
+    {
+        crosshair.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Procedures for throwing bobber
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {//Start if
+            crosshair.enabled = true;
+            BobberThrower.canThrow = true;
+        }//End if
+    }//End OnTriggerEnter
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {//Start if
+            crosshair.enabled = false;
+            BobberThrower.canThrow = false;
+        }//End if
+    }//End OnTriggerEnter
+}
